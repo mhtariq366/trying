1367ef86102c41f2f5aac4b6586a49d055b68a81
Muhammad Hassan Tariq <hassan@192.168.1.14>
2025-07-24 18:17:51 +0500

    comments

---
+++ comments.py
@@ -0,0 +1,9 @@
+#single line comment is started a # symbol
+
+var = """
+Multi line comment is done this.
+However, you can assign this multi line comment to a str as well.
+this way it can printed.
+"""
+
+print(var)
